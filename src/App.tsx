20b831e2d9bd4626814789c7b7274dead17a3590
Aamir-website <doozboi99@gmail.com>
2025-07-29 11:45:46 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -248,11 +248,11 @@ function App() {
   gsap.registerPlugin(ScrollTrigger);
 
   gsap.to(portraitRef.current, {
-  y: 7000, // ya 200 if you want smaller slide
+  y: 400, // ya 200 if you want smaller slide
   scrollTrigger: {
     trigger: heroRef.current,
     start: "top top",
-    end: "top+=15000", // 🔁 reduce to make it slower & smoother
+    end: "top+=1500", // 🔁 reduce to make it slower & smoother
     scrub: 1        // 🔁 increase for smoother animation
   }
 });
